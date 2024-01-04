08a6f64014be58743f85b5d407c7592b591587dd
lostdesire <lopkawqs@gmail.com>
2024-01-04 13:19:35 +0000

    fix: wandb project name

--- code/lightgcn/train.py
+++ code/lightgcn/train.py
@@ -15,7 +15,7 @@ logger = get_logger(logging_conf)
 
 def main(args: argparse.Namespace):
     wandb.login()
-    wandb.init(project="Level2-lightgcn", config=vars(args))
+    wandb.init(project="level2-lightgcn", config=vars(args))
     set_seeds(args.seed)
     
     use_cuda: bool = torch.cuda.is_available() and args.use_cuda_if_available
